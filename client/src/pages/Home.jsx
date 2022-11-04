2288ba881a629e7a5db98ac0d0a7c623008fe0a6
MustafaBasar471 <mustafabasar471@gmail.com>
2022-11-04 16:53:41 +0300

    some fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Posts, Sidebar } from "../components";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const res = await axios.get("/posts");
+    console.log(res);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
